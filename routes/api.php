790da1b56a220322bb72459d950ca451dd429ab3
Abraham Muthiani <muthianiabraham254@gmail.com>
2025-05-13 19:05:54 +0300

    lokeshon

--- routes/api.php
+++ routes/api.php
@@ -5,29 +5,34 @@ use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\User;
 
 // Public Routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+// Report Routes
 Route::post('/reports', [ReportController::class, 'store']);
 Route::get('/reports', [ReportController::class, 'index']);
 
+// Authenticated Routes
 Route::middleware('auth:sanctum')->group(function () {
     // Auth Routes
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
 
     // User Routes
-    Route::get('user', [UserController::class, 'index']);
-    Route::post('user', [UserController::class, 'store']);
-    Route::get('user/{id}', [UserController::class, 'show']);
-    Route::put('user/{id}', [UserController::class, 'update']);
-    Route::delete('user/{id}', [UserController::class, 'destroy']);
+    Route::get('/user', [UserController::class, 'index']);
+    Route::post('/user', [UserController::class, 'store']);
+    Route::get('/user/{id}', [UserController::class, 'show']);
+    Route::put('/user/{id}', [UserController::class, 'update']);
+    Route::delete('/user/{id}', [UserController::class, 'destroy']);
 
     // Role Routes
-    Route::post('role', [RoleController::class, 'createRole']);
-    Route::get('role', [RoleController::class, 'index']);
-    Route::get('role/{id}', [RoleController::class, 'getRole']);
-    Route::put('role/{id}', [RoleController::class, 'updateRole']);
-    Route::delete('role/{id}', [RoleController::class, 'deleteRole']);
+    Route::post('/role', [RoleController::class, 'createRole']);
+    Route::get('/role', [RoleController::class, 'index']);
+    Route::get('/role/{id}', [RoleController::class, 'getRole']);
+    Route::put('/role/{id}', [RoleController::class, 'updateRole']);
+    Route::delete('/role/{id}', [RoleController::class, 'deleteRole']);
 });
